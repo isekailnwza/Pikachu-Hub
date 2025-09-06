3cebbf0549b7d9a842522f133082d50856424084
isekailnwza <pattawakarnbas@gmail.com>
2025-09-06 13:27:06 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,4 +2,4 @@ local Game = {
     82706969044553 = "ReaperLegends"
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/isekailnwza/Pikachu-Hub/refs/heads/main/" .. [Game[game.PlaceId]]))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/isekailnwza/Pikachu-Hub/refs/heads/main/" .. [Game[game.PlaceId]]))()
