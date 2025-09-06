9c9a0c54ebdbdb94f859e7a3876f97d75e8de406
isekailnwza <pattawakarnbas@gmail.com>
2025-09-06 13:43:43 +0700

    Update and rename Loader.lua to MainLoader.lua

--- Loader.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/isekailnwza/Pikachu-Hub/refs/heads/main/" .. game.PlaceId))()
---
+++ MainLoader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/isekailnwza/Pikachu-Hub/refs/heads/main/" .. game.PlaceId .. ".lua"))()
